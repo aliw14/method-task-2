213c50a2161c845adb9c619f9edd928ecc4972c6
Mammadov Ali <AligM@code.local>
2023-01-12 19:29:00 +0400

    mesaj

---
+++ test1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reverse = new Reverse();
+
+            Console.Write("eded daxil edin:");
+            int number = int.Parse(Console.ReadLine());
+
+            int reversenumber = reverse.Number(number);
+            Console.WriteLine(reversenumber);
+
+        }
+    }
+}
---
+++ test1/Reverse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test1
+{
+    class Reverse
+    {
+        internal int Number(int number)
+        {
+            int reversenumber = 0;
+            int r;
+            int count = (int)Math.Log10(number);
+
+            while (number != 0)
+            {
+                r = number % 10;
+                number /= 10;
+                reversenumber += r * (int)Math.Pow(10, count);
+                count--;
+            }
+
+            return reversenumber;
+        }
+    }
+}
